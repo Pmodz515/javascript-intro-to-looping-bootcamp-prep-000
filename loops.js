1ee100fab0bc8b8375145100e7fbc6cf56f9b1e1
Patrick Modafferi <pat.modafferi@gmail.com>
2017-10-09 20:28:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -14,8 +14,15 @@ console.log(n--)
   }
   return "done"
 }
-function doWhileLoop(array){
+
+function doWhileLoop(array)
+{
   function maybeTrue(){
     return Math.random() >= 0.5
   }
+do{
+  array.shift()
+}
+while (array.length > 0 && maybeTrue()) 
+  return array
 }
